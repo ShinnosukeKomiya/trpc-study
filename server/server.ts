ae8d87565c99c3efdbc1e2b5f1a0e4a46a696dcf
ShinnosukeKomiya <komiya.shinnosuke@lmi.ne.jp>
2024-12-22 23:42:45 +0900

    trpc/testで実際にlogを呼び出す

--- server/server.ts
+++ server/server.ts
@@ -1,11 +1,28 @@
 import express from "express";
+import { initTRPC } from "@trpc/server";
+import * as trpcExpress from "@trpc/server/adapters/express";
 
 const app = express();
 const PORT = 5000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
+// app.get("/", (req, res) => {
+//   res.send("Hello World");
+// });
+
+const trpc = initTRPC.create();
+const router = trpc.router;
+const publicProcedure = trpc.procedure;
+
+const appRouter = router({
+  test: publicProcedure.query(() => "Query Response"),
 });
+
+app.use(
+  "/trpc",
+  trpcExpress.createExpressMiddleware({
+    router: appRouter,
+  })
+);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
